30f6930b865dd7bd5b5d3353a70a9f10c8a90e77
jetme1 <gomez_andrew@hotmail.com>
2019-09-15 22:22:32 -0400

    enter button add

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,2 +1,8 @@
+document.addEventListener("keypress", function (event) {
 
-    alert("test")
+    if (event.key == "Enter" || event.keyCode == 13) {
+        (event.preventDefault());
+
+        document.getElementById("enter").click();
+    }
+});
